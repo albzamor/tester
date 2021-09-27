ae67f1478133fe92f0d03a653acd37278e88a2fd
Alberto <fabricadenubes@gmail.com>
2021-09-27 19:41:14 +0200

    tester push_swap

---
+++ tester_push_swap/chequeo.sh
@@ -0,0 +1,3 @@
+ARG=$(cat nums.txt); ./push_swap $ARG | ./checker_Mac $ARG
+ARG=$(cat nums.txt); ./push_swap $ARG | wc -l
+
---
+++ tester_push_swap/generate_nums.sh
@@ -0,0 +1,1 @@
+seq -1000 1000 | sort -R | tail -n 3 | tr '\n' ' ' > nums.txt
